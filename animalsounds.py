b966d081dd013080457719d6ba90cf58b2a95ae8
ponleu3m <ponleu@api-school.com>
2025-07-15 18:03:13 -0700

    Update animalsounds.py

--- animalsounds.py
+++ animalsounds.py
@@ -1,14 +1,12 @@
-def animal_sound(animal):
-    if animal == "cat":
-        return "Meow"
-    elif animal == "dog":
-        return "Woof"
-    elif animal == "cow":
-        return "Moo"
-    elif animal == "duck":
-        return "Quack"
-    else:
-        return "I don't know that animal."
-
 animal = input("Enter an animal (cat, dog, cow, duck): ").lower()
-print(animal_sound(animal))
+
+if animal == "cat":
+    print("Meow")
+elif animal == "dog":
+    print("Woof")
+elif animal == "cow":
+    print("Moo")
+elif animal == "duck":
+    print("Quack")
+else:
+    print("I don't know that animal")
